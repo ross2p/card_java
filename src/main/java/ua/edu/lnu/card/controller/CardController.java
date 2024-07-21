88e492e10b52bfcf044e53c97eb5d740cce947fc
Rostyslav <rostyslavpasternak@gmail.com>
2024-07-21 22:06:05 +0300

    commit

--- src/main/java/ua/edu/lnu/card/controller/CardController.java
+++ src/main/java/ua/edu/lnu/card/controller/CardController.java
@@ -1,16 +1,74 @@
 package ua.edu.lnu.card.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import ua.edu.lnu.card.dto.card.CardCreationUpdateRequest;
+import ua.edu.lnu.card.dto.card.CardResponse;
 import ua.edu.lnu.card.service.CardService;
+
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users/{userId}/decks/{deckId}/cards")
+@RequestMapping("/api/user/{userId}/deck/{deckId}")
 public class CardController {
     private final CardService cardService;
+
+    @GetMapping("card/{cardId}")
+    @PreAuthorize("@auth.isMe(#userId) or @auth.isCollaborator(#deckId) or hasRole('ADMIN')")
+    public @ResponseBody ResponseEntity<CardResponse> getCard(@PathVariable Long userId,
+                                                              @PathVariable Long deckId,
+                                                              @PathVariable Long cardId) {
+        CardResponse card = cardService.readById(cardId);
+        return ResponseEntity.ok(card);
+    }
+
+    @GetMapping("cards")
+    @PreAuthorize("@auth.isMe(#userId) or @auth.isCollaborator(#deckId) or hasRole('ADMIN')")
+    public @ResponseBody ResponseEntity<Page<CardResponse>> getAllCards(@PathVariable Long userId,
+                                                                        @PathVariable Long deckId,
+                                                                        @RequestParam(value = "offset", required = false) Integer offset,
+                                                                        @RequestParam(value = "pageSize", required = false) Integer pageSize,
+                                                                        @RequestParam(value = "sortBy", required = false) String sortBy) {
+        if(null == offset) offset = 0;
+        if(null == pageSize) pageSize = 10;
+        if(StringUtils.isEmpty(sortBy)) sortBy = "id";
+        PageRequest pageRequest = PageRequest.of(offset, pageSize, Sort.by(sortBy));
+        Page<CardResponse> cards = cardService.getAllByDeckId(deckId, pageRequest);
+        return ResponseEntity.ok(cards);
+    }
+    @PostMapping("card")
+    @PreAuthorize("@auth.isMe(#userId) or @auth.isCollaborator(#deckId) or hasRole('ADMIN')")
+    public @ResponseBody ResponseEntity<CardResponse> createCard(@PathVariable Long userId,
+                                                                 @PathVariable Long deckId,
+                                                                 @RequestBody CardCreationUpdateRequest cardDto) {
+        CardResponse createdCard = cardService.create(deckId, cardDto);
+        return ResponseEntity.ok(createdCard);
+    }
+    @PutMapping("card/{cardId}")
+    @PreAuthorize("@auth.isMe(#userId) or @auth.isCollaborator(#deckId) or hasRole('ADMIN')")
+    public @ResponseBody ResponseEntity<CardResponse> updateCard(@PathVariable Long userId,
+                                                                 @PathVariable Long deckId,
+                                                                 @PathVariable Long cardId,
+                                                                 @RequestBody CardCreationUpdateRequest cardDto) {
+        CardResponse updatedCard = cardService.update(cardId, cardDto);
+        return ResponseEntity.ok(updatedCard);
+    }
+
+    @DeleteMapping("card/{cardId}")
+    @PreAuthorize("@auth.isMe(#userId) or @auth.isCollaborator(#deckId) or hasRole('ADMIN')")
+    public @ResponseBody ResponseEntity<Void> deleteCard(@PathVariable Long userId,
+                                                         @PathVariable Long deckId,
+                                                         @PathVariable Long cardId) {
+        cardService.delete(cardId);
+        return ResponseEntity.noContent().build();
+    }
+
 
 
 }
